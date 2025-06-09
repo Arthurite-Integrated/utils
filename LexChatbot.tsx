594209bd6ab5151e77070a52517e564d059915c7
Abolaji Abdulrahman Alabi <101397467+Abolaj@users.noreply.github.com>
2025-06-09 09:48:02 +0100

    Update LexChatbot.tsx

--- LexChatbot.tsx
+++ LexChatbot.tsx
@@ -18,14 +18,14 @@ export default function LexChatbot() {
   useEffect(() => {
     // Create and append the script tag
     const script = document.createElement('script');
-    script.src = 'https://cloudfront-snippet...';
+    script.src = '"https://d1mg0dzt7pkrpa.cloudfront.net/lex-web-ui-loader.min.js"';
     script.async = true;
     
     // Initialize the chatbot when the script loads
     script.onload = () => {
       if (window.ChatBotUiLoader) {
         const loaderOpts = {
-          baseUrl: 'https://cloudfront-snippet...',
+          baseUrl: 'https://d1mg0dzt7pkrpa.cloudfront.net',
           shouldLoadMinDeps: true
         };
         
